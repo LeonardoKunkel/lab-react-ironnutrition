6b7d3b0324bcbc76a3d67dd4011d816cc5fc9343
Leonardo <mr_kunkel@hotmail.com>
2022-02-24 18:50:33 -0600

    Iteration 4 paused

--- src/App.js
+++ src/App.js
@@ -3,27 +3,41 @@ import './App.css';
 import FoodBox from './components/FoodBox';
 
 import foods from './foods.json';
+import AddFoodForm from './components/AddFoodForm';
+
+import { Row, Divider, Button } from 'antd';
 
 function App() {
   return (
     <>
-      <h2> Food List </h2>
-      <hr />
-      {
-        foods.map((e) => {
-          return(
+      <AddFoodForm />
 
-            <FoodBox
-              food={ {
-                name: e.name,
-                calories: e.calories,
-                image: e.image,
-                servings: e.servings
-              }}
-            />
-          )
-        })
-      }
+      <Button> Hide Form / Add New Food </Button>
+
+      {/* Display Search component here */}
+
+      <Divider>Food List</Divider>
+
+      <Row style={{ width: '100%', justifyContent: 'center' }}>
+
+        {
+          foods.map((e, index) => {
+            return(
+
+              <FoodBox
+                key={index}
+                food={ {
+                  name: e.name,
+                  calories: e.calories,
+                  image: e.image,
+                  servings: e.servings
+                }}
+              />
+            )
+          })
+        }
+
+        </Row>
     </>
   );
 }
---
+++ src/components/AddFoodForm.js
@@ -0,0 +1,69 @@
+import React from 'react'
+
+import { useState } from 'react';
+
+import { Divider, Input } from 'antd';
+
+export default function AddFoodForm(props) {
+
+    const [newFood, setNewFood] = useState({
+        name: '',
+        image: '',
+        calories: '',
+        servings: ''
+    })
+
+    const [list, setList] = useState(props.food)
+
+    const check = (event) => {
+
+        console.log(event.target.value);
+        console.log('El campo de texto en el que estás escribiendo es: ', event.target.name);
+
+        setNewFood({
+            ...newFood,
+            [event.target.name]: event.target.value
+        })
+
+    }
+
+    const submit = (event) => {
+
+        event.preventDefault()
+
+        setList({
+            ...list,
+            newFood
+        })
+
+        setNewFood({
+            name: '',
+            image: '',
+            calories: '',
+            servings: ''
+        })
+    }
+    return (
+        <form
+            onSubmit={ (event) => { submit(event) } }
+            style={ {padding: '0 45px 20px 45px'} }
+        >
+
+            <Divider>Add Food Entry</Divider>
+
+            <label>Name</label>
+            <Input name="name" value={newFood.name} type="text" onChange={(event) => { check(event) }} />
+
+            <label>Image</label>
+            <Input name="image" value={newFood.image} type="text" onChange={(event) => { check(event) }} />
+
+            <label>Calories</label>
+            <Input name="calories" value={newFood.calories} type="text" onChange={(event) => { check(event) }} />
+
+            <label>Servings</label>
+            <Input name="servings" value={newFood.servings} type="text" onChange={(event) => { check(event) }} />
+
+            <button type="submit">Create</button>
+        </form>
+    )
+}
